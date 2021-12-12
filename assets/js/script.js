36529d9eaaa52f882532a5d548ac5b4618852468
Lo Selby <laurenxselby@gmail.com>
2021-12-11 22:24:17 -0800

    added moment for today's date -utilized api fetch to grab temp, wind, humidty for today's forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,65 @@
 // declare variables related to API call
-var APIkey = "7bf5850a0af8a9b096b521ae4589d3bf";
-var cityState;
-var requestURL ="http://api.openweathermap.org/data/2.5/weather?q=" + cityState + "&appid=" + APIkey;
+var zipInputEl = document.querySelector('#zipcode');
+var zipFormEl = document.querySelector('#zip-form');
+var todayTemp = document.querySelector('#today-temp');
+var todayWind = document.getElementById("#today-wind");
+var todayHumidity = document.getElementById("#today-humidity");
+var todayUV = document.getElementById("#today-uv");
+var todaysDateEl = document.querySelector('#todays-date');
 
-fetch(requestURL) 
-    .then(function (response) {
-        return response.JSON ();
-    })
-    .then(function(data) {
-        console.log(data)
-    })
+// moment in time for today's forecast
+var today = moment();
+todaysDateEl.textContent = (today.format("dddd, MMMM Do YYYY"));
+
+
+
+var zipFormSubmit = function (event) {
+    event.preventDefault();
+  
+    var zipcode = zipInputEl.value.trim();
+  
+    if (zipcode) {
+      getWeatherData(zipcode);
+  
+      repoContainerEl.textContent = '';
+      nameInputEl.value = '';
+    } else {
+      alert('Please enter a GitHub username');
+    }
+  };
+  
+
+
+
+
+var getWeatherData = function (weather) {
+    var APIkey = "7bf5850a0af8a9b096b521ae4589d3bf";
+    var zipcode = zipInputEl.value.trim();
+    var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + '&appid=' + APIkey;
+    
+    fetch(requestUrl)
+      .then(function (response) {
+        if (response.ok) {
+          console.log(response);
+          response.json().then(function (data) {
+            console.log(data);
+            // displayForecast(data, weather);
+            todayTemp.textContent = "Temperature: " + Math.floor((data.main.temp - 273.15) * 1.8 + 32) + "°F";
+
+            todayWind.textContent = 'Wind: ' + Math.floor(data.wind.speed * 2.237) + 'mph';
+            todayHumidity = 'Humidity: ' + data.main.humidity + '%';
+            // todayUV = 'UV Index: ' + data.;
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to recgonize zip code');
+      });
+  };
+
+ 
+            // "Temperature: " + Math.floor(data.main.temp - kelvin * 1.8 + 32) + "°F";
+
+zipFormEl.addEventListener("submit", getWeatherData);
