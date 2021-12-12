e260edc43843c86434583aa58010ff5904e2fd30
Lo Selby <laurenxselby@gmail.com>
2021-12-11 19:58:54 -0800

    fixed column issue with card and accordian -edited search history buttons -started fetch function in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 // declare variables related to API call
 var APIkey = "7bf5850a0af8a9b096b521ae4589d3bf";
 var cityState;
-var queryURL ="http://api.openweathermap.org/data/2.5/weather?q=" + cityState + "&appid=" + APIkey;
+var requestURL ="http://api.openweathermap.org/data/2.5/weather?q=" + cityState + "&appid=" + APIkey;
 
-fetch(queryURL)
+fetch(requestURL) 
+    .then(function (response) {
+        return response.JSON ();
+    })
+    .then(function(data) {
+        console.log(data)
+    })
